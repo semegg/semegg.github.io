11f45c7e547066591529d35d7119de09d0bbc615
semegg <semeg03@gmail.com>
2023-07-05 22:45:22 +0300

    try new js

--- web_app.js
+++ web_app.js
@@ -8,9 +8,7 @@ tg.MainButton.color = "#2cab37";
 let items = "";
 
 let btn_amrcn = document.getElementById("btn_amrcn");
-let btn_esprss = document.getElementById("btn_esprss");
 let btn_dpp = document.getElementById("btn_dpp");
-let btn_ltt = document.getElementById("btn_ltt");
 
 btn_amrcn.addEventListener("click", function() {
     if (tg.MainButton.isVisible) {
@@ -18,19 +16,8 @@ btn_amrcn.addEventListener("click", function() {
     }
     else {
     items = "amrcn";
-    tg.MainButton.setText("Вы выбрали " items "товаров!");
+    tg.MainButton.setText("Вы выбрали товаров!");
 
-    tg.MainButton.show();
-    }
-});
-
-btn_esprss.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-    }
-    else {
-    items = "amrcn";
-    tg.MainButton.setText("Вы выбрали " items "товаров!");
     tg.MainButton.show();
     }
 });
@@ -40,33 +27,13 @@ btn_dpp.addEventListener("click", function() {
     tg.MainButton.hide();
     }
     else {
-    items = "amrcn";
+    items = "dpp";
     tg.MainButton.setText("Вы выбрали товаров!");
     tg.MainButton.show();
     }
 });
 
-btn_ltt.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-    }
-    else {
-    items = "amrcn";
-    tg.MainButton.setText("Вы выбрали " items "товаров!");
-    tg.MainButton.show();
-    }
-});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
     tg.sendData(items);
 });
-
-
-
-let usercard = document.getElementById("usercard");
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDateUnsafe.user.first_name} 
-${tg.initDateUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
