f1d2d08eed7f635fb6b9a6072607ddabb2a90556
semegg <semeg03@gmail.com>
2023-07-05 23:31:59 +0300

    try new js

--- web_app.js
+++ web_app.js
@@ -2,22 +2,33 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
+
 let items = [];
+
+// Восстановление значения items при загрузке страницы
+if (window.location.pathname === "/") {
+  const savedItems = localStorage.getItem("items");
+  if (savedItems) {
+    items = JSON.parse(savedItems);
+  }
+}
+
 let buttons = document.querySelectorAll('[id^="btn_"]');
 buttons.forEach(button => {
   button.addEventListener("click", function() {
-
-      const buttonId = button.id.slice(4);
-      items.push(buttonId);
-      const count = items.length;
-      tg.MainButton.setText(`Вы выбрали ${count} товаров!`);
-      tg.MainButton.show();
+    const buttonId = button.id.slice(4);
+    items.push(buttonId);
+    const count = items.length;
+    tg.MainButton.setText(`Вы выбрали ${count} товаров!`);
+    tg.MainButton.show();
   });
 });
+
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-        if (window.location.pathname === "/menu.html") {
-        window.location.href = "/";
-         } else if (window.location.pathname === "/index.html") {
-        tg.sendData(items);
-      }
+  if (window.location.pathname === "/menu.html") {
+    localStorage.setItem("items", JSON.stringify(items));
+    window.location.href = "/";
+  } else if (window.location.pathname === "/index.html") {
+    tg.sendData(items);
+  }
 });
