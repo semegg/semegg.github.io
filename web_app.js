619b49b9cdd52964cad835833ecfd985a9e5b03c
semegg <semeg03@gmail.com>
2023-07-05 23:25:38 +0300

    try new js

--- web_app.js
+++ web_app.js
@@ -2,24 +2,22 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
-
 let items = [];
-
 let buttons = document.querySelectorAll('[id^="btn_"]');
 buttons.forEach(button => {
   button.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-      tg.MainButton.hide();
-    } else {
+
       const buttonId = button.id.slice(4);
       items.push(buttonId);
-
-      // Проверка текущего URL
-      if (window.location.pathname === "/menu.html") {
-        window.location.href = "/index.html";
-      } else if (window.location.pathname === "/index.html") {
+      const count = items.length;
+      tg.MainButton.setText(`Вы выбрали ${count} товаров!`);
+      tg.MainButton.show();
+  });
+});
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+        if (window.location.pathname === "/menu.html") {
+        window.location.href = "/";
+         } else if (window.location.pathname === "/index.html") {
         tg.sendData(items);
       }
-    }
-  });
 });
