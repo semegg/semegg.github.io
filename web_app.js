44566bfc7dba30ba652a3c44915c6fe538222218
semegg <semeg03@gmail.com>
2023-07-05 22:52:14 +0300

    try new js

--- web_app.js
+++ web_app.js
@@ -1,39 +1,23 @@
 let tg = window.Telegram.WebApp;
-
 tg.expand();
-
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
 let items = "";
 
-let btn_amrcn = document.getElementById("btn_amrcn");
-let btn_dpp = document.getElementById("btn_dpp");
-
-btn_amrcn.addEventListener("click", function() {
+let buttons = document.querySelectorAll('[id^="btn_"]');
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
     if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
+      tg.MainButton.hide();
+    } else {
+      items = button.id.slice(4); // Получаем значение после "btn_" в id кнопки
+      tg.MainButton.setText("Вы выбрали товаров!");
+      tg.MainButton.show();
     }
-    else {
-    items = "amrcn";
-    tg.MainButton.setText("Вы выбрали товаров!");
-
-    tg.MainButton.show();
-    }
+  });
 });
 
-btn_dpp.addEventListener("click", function() {
-    if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-    }
-    else {
-    items = "dpp";
-    tg.MainButton.setText("Вы выбрали товаров!");
-    tg.MainButton.show();
-    }
-});
-
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData(items);
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+  tg.sendData(items);
 });
