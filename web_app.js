d32f922207ab245ac104eef302ef1534688c506e
semegg <semeg03@gmail.com>
2023-07-22 19:40:52 +0300

    как заебали пустые сообщения о комите

--- web_app.js
+++ web_app.js
@@ -1,26 +1,18 @@
-
   // Код з buttons.js
   const items = document.body.getElementsByClassName("item");
-
   for (let item of Array.from(items)) {
     const counter = item.getElementsByClassName("counter")[0];
     counter.style.setProperty("visibility", "hidden");
-
     const table = item.getElementsByClassName("table")[0];
     table.style.setProperty("display", "none");
     const row = table.getElementsByClassName("row")[0];
-
     const inc = row.getElementsByClassName("plus")[0];
     inc.addEventListener("click", () => increment(item));
-
     const dec = row.getElementsByClassName("minus")[0];
     dec.addEventListener("click", () => decrement(item));
-
     const add = item.getElementsByClassName("add")[0];
     add.addEventListener("click", () => increment(item));
-
     const initialValue = localStorage.getItem(`counter_${item.id}`);
-
     if (initialValue > 0) {
       counter.style.setProperty("visibility", "visible");
       counter.textContent = Number(initialValue);
@@ -28,29 +20,23 @@
       item.getElementsByClassName("add")[0].style.setProperty("display", "none");
     }
   }
-
   const increment = (item) => {
     const counter = item.getElementsByClassName("counter")[0];
     let value = Number(counter.textContent);
-
     if (!value) {
       counter.style.setProperty("visibility", "visible");
       item.getElementsByClassName("table")[0].style.setProperty("display", "block");
       item.getElementsByClassName("add")[0].style.setProperty("display", "none");
     }
-
     value += 1;
     counter.textContent = value;
     localStorage.setItem(`counter_${item.id}`, value);
-
     // Код з web_app.js
     let tg = window.Telegram.WebApp;
     tg.expand();
     tg.MainButton.textColor = "#FFFFFF";
     tg.MainButton.color = "#2cab37";
-
     let items = [];
-
     const savedItems = localStorage.getItem("items");
     if (savedItems) {
       items = JSON.parse(savedItems);
@@ -62,7 +48,6 @@
       }
       tg.MainButton.show();
     }
-
     let buttons = document.querySelectorAll('[id^="btn_"]');
     buttons.forEach((button) => {
       button.addEventListener("click", function () {
@@ -85,17 +70,14 @@
       }
     });
   };
-
   const decrement = (item) => {
     const counter = item.getElementsByClassName("counter")[0];
     let value = Number(counter.textContent);
-
     if (!(value - 1)) {
       counter.style.setProperty("visibility", "hidden");
       item.getElementsByClassName("table")[0].style.setProperty("display", "none");
       item.getElementsByClassName("add")[0].style.setProperty("display", "block");
     }
-
     value -= 1;
     counter.textContent = value;
     localStorage.setItem(`counter_${item.id}`, value);
